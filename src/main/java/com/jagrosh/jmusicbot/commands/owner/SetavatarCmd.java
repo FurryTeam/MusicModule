2454bff9f01fe86ff51390ea0095b0a3ba20d44a
Rxflex <39628283+Rxflex@users.noreply.github.com>
2019-12-15 15:34:38 +0200

    Update SetavatarCmd.java

--- src/main/java/com/jagrosh/jmusicbot/commands/owner/SetavatarCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/SetavatarCmd.java
@@ -32,7 +32,7 @@ public class SetavatarCmd extends OwnerCommand
     public SetavatarCmd(Bot bot)
     {
         this.name = "setavatar";
-        this.help = "sets the avatar of the bot";
+        this.help = "устанавливает мне аватарку";
         this.arguments = "<url>";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.guildOnly = false;
@@ -52,16 +52,16 @@ public class SetavatarCmd extends OwnerCommand
         InputStream s = OtherUtil.imageFromUrl(url);
         if(s==null)
         {
-            event.reply(event.getClient().getError()+" Invalid or missing URL");
+            event.reply(event.getClient().getError()+" Не правильная или отстутствующая ссылка");
         }
         else
         {
             try {
             event.getSelfUser().getManager().setAvatar(Icon.from(s)).queue(
-                    v -> event.reply(event.getClient().getSuccess()+" Successfully changed avatar."), 
-                    t -> event.reply(event.getClient().getError()+" Failed to set avatar."));
+                    v -> event.reply(event.getClient().getSuccess()+" Успешная смена аватарки."), 
+                    t -> event.reply(event.getClient().getError()+" Ошибка установки аватарки."));
             } catch(IOException e) {
-                event.reply(event.getClient().getError()+" Could not load from provided URL.");
+                event.reply(event.getClient().getError()+" Не могу загрузить выбранную ссылку.");
             }
         }
     }
